367b283f00460d34275b852b4053e021da296420
PamTozier <pamela.tozier@colorado.edu>
2013-10-19 17:28:13 -0600

    now can edit items with double click-Sean
    Need to review Matt's syntax for where to put the event handlers, so
    study!!

--- todo.js
+++ todo.js
@@ -1,14 +1,18 @@
 var todoapp = {
   init: function()  {
     $('.todolist .todoItem:first-child input').keypress(todoapp.newTask);  
-  },
-  
+    $('.todolist').on('dblclick', '.item input', todoapp.beginEditTask);
+    },
   newTask: function(ev) {
     
   //get input, append to task list
     if(ev.which === 13) {
     var input = $(this);
+      
     var clonedItem = input.parent().parent().clone();
+    
+      console.log(clonedItem);  
+      
     $('.todolist').append(clonedItem);
     input.val(''); }; 
     $(clonedItem).find('.item input').attr('readonly', true);
@@ -25,7 +29,7 @@ var todoapp = {
     //top level change status for all
   },
   beginEditTask: function(ev){
-  //$(ev.target).ondblclick('input').removeAttr('readonly');
+  $(ev.currentTarget).removeAttr('readonly');
     //double click to edit items
 //    $('input').ondblclick(this:'readonly', false);
   },
